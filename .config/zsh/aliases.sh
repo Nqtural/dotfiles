f2821c33faa7cc8c7ba9dc038ae3efc614edfbf6
Nqtural <robin.stalstad@outlook.com>
2023-01-13 19:01:23 +0100

    Fixed aliases symbolic link

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -1,1 +1,16 @@
-/home/nqtural/.config/zsh/aliases.sh
+# General
+alias l="exa --icons --no-time --no-user -al"
+alias ls="exa --icons"
+alias la="exa --icons -a"
+alias tree="exa --icons -a -tree"
+alias cmatrix="cmatrix -boau1"
+alias n="newsboat"
+alias gittoken="cat ~/.gittoken | xclip -sel c"
+
+# Python programs
+alias passphrase="python ~/coding/python/passphrase-generator/main.py"
+alias pyoverlay="python ~/coding/python/PyOverlay/PyOverlay.py"
+alias weather="python ~/coding/python/WeatherFetch/main.py"
+alias ralgs="python ~/coding/python/ralgs/main.py"
+alias alarm="python ~/coding/python/alarm/main.py"
+alias gas="python ~/coding/python/gas-price-scraper/main.py"
