16cabd861fc2b25ac8b4629541334a07034c7975
Nqtural <robin.stalstad@outlook.com>
2023-09-29 17:36:15 +0200

    updated nvim config

--- .config/nvim/lua/user/remap.lua
+++ .config/nvim/lua/user/remap.lua
@@ -1,6 +1,10 @@
 -- Set leader key
 vim.g.mapleader = " "
 
+-- Skeletons
+vim.keymap.set("n", ",html", ":-1read $HOME/.config/nvim/skeletons/skeleton.html<CR>3jwf>a")
+vim.keymap.set("n", ",py", ":-1read $HOME/.config/nvim/skeletons/skeleton.py<CR>jA")
+
 -- NvimTree
 vim.keymap.set("n", "<leader>o", vim.cmd.NvimTreeToggle)
 
@@ -14,6 +18,23 @@ vim.keymap.set("n", "<leader>r", ":Telescope oldfiles<CR>")
 -- Commentary
 vim.keymap.set("n", "<leader>c", ":Commentary<CR>")
 vim.keymap.set("v", "<leader>c", ":'<,'>Commentary<CR>")
+
+-- Get rid of arrow keys
+-- Insert
+vim.keymap.set("i", "<Up>", "<nop>")
+vim.keymap.set("i", "<Right>", "<nop>")
+vim.keymap.set("i", "<Down>", "<nop>")
+vim.keymap.set("i", "<Left>", "<nop>")
+-- Normal
+vim.keymap.set("n", "<Up>", "<nop>")
+vim.keymap.set("n", "<Right>", "<nop>")
+vim.keymap.set("n", "<Down>", "<nop>")
+vim.keymap.set("n", "<Left>", "<nop>")
+-- Visual
+vim.keymap.set("v", "<Up>", "<nop>")
+vim.keymap.set("v", "<Right>", "<nop>")
+vim.keymap.set("v", "<Down>", "<nop>")
+vim.keymap.set("v", "<Left>", "<nop>")
 
 -- Move lines in visual mode with K and J
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
